809da4940cf4761c87bc020e429345caaa36389a
Bill Bitok <98650586+Billy272@users.noreply.github.com>
2022-12-13 03:34:49 +0000

    const

---
+++ rev/new.cpp
@@ -0,0 +1,24 @@
+class BankAccount {
+private:
+    double balance;
+    std::string accountNumber;
+
+public:
+    // Constructor
+    BankAccount(double initialBalance, std::string accountNumber) {
+        balance = initialBalance;
+        this->accountNumber = accountNumber;
+    }
+};
+class BankAccount {
+private:
+    double balance;
+    std::string accountNumber;
+
+public:
+    // Constructor
+    BankAccount(double initialBalance, std::string accountNumber) {
+        balance = initialBalance;
+        this->accountNumber = accountNumber;
+    }
+};
