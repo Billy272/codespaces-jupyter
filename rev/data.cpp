770f54d28ee28ec9a70b77fea37e05e8a3dffb43
Bill Bitok <98650586+Billy272@users.noreply.github.com>
2022-12-14 13:25:37 +0000

    incomplete

---
+++ rev/data.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class mat
+{
+    mat A[3][3],B[3][3],C[3][3],R,C;
+    public:
+      void input();
+      void display();
+}
+
+void mat::input()
+{
+
+}
+void mat::display()
+{
+
+}
+
+int main()
+{
+  mat T;
+  T.input();
+  T.display();
+  return 0;
+}
